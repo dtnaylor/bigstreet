2d06346eae58fdf1018779a7274ff561f18beec7
Zakir Durumeric <zakird@gmail.com>
2010-11-28 23:40:30 -0600

    changing database configuration such that project can be jarified and include the database configuration.... and cleaning bizarro conf-loading code.

--- LinkagesViewer/src/edu/uiowa/nursing/configuration/DBConnection.java
+++ LinkagesViewer/src/edu/uiowa/nursing/configuration/DBConnection.java
@@ -7,14 +7,37 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public abstract class DBConnection {
 	public static Connection connection;
 	private static String address, username, password, database;
 	
 	public static void openConnection()
 	{
-		getDBInfo();
 		
+		try {
+			java.net.URL url =  ClassLoader.getSystemResource("db.ini");
+		    BufferedReader input =  new BufferedReader(new FileReader(url.getFile()));
+		    try {
+		    	String line = null;
+		        while ((line = input.readLine()) != null) {
+		        	String[] splitLine = line.split("=");
+		        	if(splitLine[0].equals("address"))
+		        		address = splitLine[1];
+		        	else if (splitLine[0].equals("username"))
+		        		username = splitLine[1];
+		        	else if (splitLine[0].equals("password"))
+		        		password = splitLine[1];
+		        	else if (splitLine[0].equals("database"))
+		        		database = splitLine[1];
+		        }
+		    }
+		    finally {
+		    	input.close();
+		    }
+		} catch (IOException ex){
+		      ex.printStackTrace();
+		}
 		try {
 			connection = DriverManager.getConnection("jdbc:sqlserver:" + address 
 					+ ";user=" + username 
@@ -32,37 +55,7 @@ public abstract class DBConnection {
 		try {
 			connection.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	
-	private static void getDBInfo()
-	{
-		try {
-			  String homeDir = System.getProperty("user.home");
-		      BufferedReader input =  new BufferedReader(new FileReader(homeDir + "/Documents/nnndb.conf"));
-		      try {
-		        String line = null;
-		        while (( line = input.readLine()) != null){
-		        	String[] splitLine = line.split("=");
-		        	if(splitLine[0].equals("address"))
-		        		address = splitLine[1];
-		        	else if (splitLine[0].equals("username"))
-		        		username = splitLine[1];
-		        	else if (splitLine[0].equals("password"))
-		        		password = splitLine[1];
-		        	else if (splitLine[0].equals("database"))
-		        		database = splitLine[1];
-		        }
-		      }
-		      finally {
-		        input.close();
-		      }
-		    }
-		    catch (IOException ex){
-		      ex.printStackTrace();
-		    }
 	}
 }
